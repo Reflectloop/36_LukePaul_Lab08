fcaa57d24e159cec6aed1531b019898e23adf148
vaCleaner <155930075+vaCleaner@users.noreply.github.com>
2024-01-22 12:35:38 +0800

    done

--- 37_LukePaul_Lab08/Assets/Scripts/Manager.cs
+++ 37_LukePaul_Lab08/Assets/Scripts/Manager.cs
@@ -8,10 +8,17 @@ public class Manager : MonoBehaviour
 
     public float SGDUSD = 0.74f;
     public float SGDJPY = 82.78f;
+    public float SGDRM = 3.08f;
+    public float SGDEUR = 0.63f;
+    public float SGDKRW = 881.54f;
+    public float SGDTWD = 20.73f;
+
+    public int COUNTER;
+    public int numOFTick;
+
     public float value;
 
-    public Toggle SGD2USD;
-    public Toggle SGD2JPY;
+    public Toggle[] allToggle;
 
     float SGDAmount;
 
@@ -19,12 +26,12 @@ public class Manager : MonoBehaviour
 
     public Text ConvertedAmount;
     public Text Error;
+
     // Start is called before the first frame update
     void Start()
     {
         Error.gameObject.SetActive(false);
-        SGD2USD.isOn = false;
-        SGD2JPY.isOn = false;
+
     }
 
     // Update is called once per frame
@@ -35,33 +42,88 @@ public class Manager : MonoBehaviour
 
     public void convertamount()
     {
-        SGDAmount = float.Parse(thxamount.text);
+        numOFTick = 0;
+        try
+        {
+          SGDAmount = float.Parse(thxamount.text);
+            for (int i = 0; i < allToggle.Length; i++)
+            {
+               
+                if (allToggle[i].isOn == true)
+                {
+                    numOFTick++;
+                }
+                if (numOFTick > 1)
+                {
+                    Error.gameObject.SetActive(true);
+                    Error.text = "Choose one only 1";
+                    return;
+                }
 
-        if (SGD2USD.GetComponent<Toggle>().isOn == true && SGD2JPY.GetComponent<Toggle>().isOn == false)
-        {
-            Error.gameObject.SetActive(false);
-            value = SGDAmount * SGDUSD;
-            ConvertedAmount.text = "$" + value.ToString() + " USD";
+            }
+           
+
+                if (allToggle[0].isOn == true)
+                {
+                     Error.gameObject.SetActive(false);
+                     value = SGDAmount * SGDUSD;
+                    ConvertedAmount.text = "$" + value.ToString() + " USD";
+
+                }
+
+                if (allToggle[1].isOn == true)
+                {
+                    Error.gameObject.SetActive(false);
+                    value = SGDAmount * SGDJPY;
+                    ConvertedAmount.text = "$" + value.ToString() + " JPY";
+                }
+                if (allToggle[2].isOn == true)
+                {
+                       Error.gameObject.SetActive(false);
+                   value = SGDAmount * SGDRM;
+                    ConvertedAmount.text =  value.ToString() + "RINGGIT";
+
+                }
+            if (allToggle[3].isOn == true)
+            {
+                Error.gameObject.SetActive(false);
+                value = SGDAmount * SGDEUR;
+               ConvertedAmount.text = value.ToString() + "EUR";
+            }
+            if (allToggle[4].isOn == true)
+            {
+                Error.gameObject.SetActive(false);
+                value = SGDAmount * SGDKRW;
+                ConvertedAmount.text = value.ToString() + "KRW";
+            }
+            if (allToggle[5].isOn == true)
+            {
+                Error.gameObject.SetActive(false);
+                value = SGDAmount * SGDTWD;
+                ConvertedAmount.text = value.ToString() + "TWD";
+            }
         }
-        if (SGD2JPY.GetComponent<Toggle>().isOn == true && SGD2USD.GetComponent<Toggle>().isOn == false)
-        {
-            Error.gameObject.SetActive(false);
-            value = SGDAmount * SGDJPY;
-            ConvertedAmount.text = "¥" + value.ToString() + " JPY";
-        }
-        if (SGD2JPY.GetComponent<Toggle>().isOn == true && SGD2USD.GetComponent<Toggle>().isOn == true)
+        catch
         {
             Error.gameObject.SetActive(true);
-            Error.text = "Invalid Please Tick 1 ";
+            Error.text = "INPUT ERROR ";
+
+
+
         }
+       
+       
 
     }
     public void clear()
     {
         thxamount.text = "";
         ConvertedAmount.text = "";
+        for (int i = 0; i < allToggle.Length; i++)
+        {
+            allToggle[i].isOn = false;
 
-        SGD2USD.isOn = false;
-        SGD2JPY.isOn = false;
+        }
+
     }
 }
